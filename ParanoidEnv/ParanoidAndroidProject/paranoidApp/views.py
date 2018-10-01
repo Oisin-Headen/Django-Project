25629b741f31a9c2f56daabc7e03871cd8019bc7
Oisín Headen <s3513821@student.rmit.edu.au>
2018-10-02 09:15:31 +1000

    Adding survey choice to home page
    Currently, will look at the provided ID, and declare invalid, valid or not found.
    1 is currently valid for testing purposes, but there is no data yet

--- ParanoidEnv/ParanoidAndroidProject/paranoidApp/views.py
+++ ParanoidEnv/ParanoidAndroidProject/paranoidApp/views.py
@@ -3,7 +3,17 @@ from django.http import HttpResponse
 from django.template import loader
 
 
-def index(request):
-    """The Index Page"""
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return HttpResponse(loader.get_template("paranoidApp/index.html").render({}, request))
+# def index(request):
+#     """The Index Page"""
+#     # return HttpResponse("Hello, world. You're at the polls index.")
+#     return HttpResponse(loader.get_template("paranoidApp/index.html").render({}, request))
+
+def index(request, question_id=-1):
+    """The Index Page, with a survey id"""
+    if question_id == -1:
+        context = {'status': "invalid"}
+    elif question_id == 1:
+        context = {'status': "valid"}
+    else:
+        context = {'status': "not_found"}
+    return HttpResponse(loader.get_template("paranoidApp/index.html").render(context, request))
