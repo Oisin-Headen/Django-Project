f8ce865198d0ac989fef317abec11b78673b5948
Oisín Headen <s3513821@student.rmit.edu.au>
2018-10-02 11:06:47 +1000

    basic database (not yet migrated)
    Having trouble with the question types, not sure how to bring the different types into one model.

--- ParanoidEnv/ParanoidAndroidProject/paranoidApp/models.py
+++ ParanoidEnv/ParanoidAndroidProject/paranoidApp/models.py
@@ -2,3 +2,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Survey(models.Model):
+    """Model for the surveys"""
+    survey_desc = models.CharField(max_length=500)
+    survey_create_date = models.DateField('date created')
+
+class QuestionType(models.Model):
+    """The Question types:
+    Free form text,
+    Multiple Choice Radio buttons
+    Multiple choice checkbox
+    """
+    question_type_name = models.CharField(max_length=200)
+
+class Question(models.Model):
+    """Model for the questions"""
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_name = models.CharField(max_length=200)
+    question_desc = models.CharField(max_length=500)
+    question_required = models.BooleanField(default=False)
