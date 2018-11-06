05dfc397dcecbbd8c5fad5f2a47c635994e53e96
Oisín Headen <s3513821@student.rmit.edu.au>
2018-11-06 11:42:07 +1100

    Removed "is admin" box at signup

--- ParanoidEnv/ParanoidAndroidProject/paranoidApp/forms.py
+++ ParanoidEnv/ParanoidAndroidProject/paranoidApp/forms.py
@@ -7,8 +7,7 @@ class SignUpForm(UserCreationForm):
     """Sign up Form"""
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    is_admin = forms.BooleanField(required=False, help_text="Are you an Admin?")
 
     class Meta:
         model = SurveyUser
-        fields = ('username', 'first_name', 'last_name', 'is_admin', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', )
