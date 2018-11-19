c7e51888ed720bb3c6903a73e4791ef4c788e30a
Oisín Headen <s3513821@student.rmit.edu.au>
2018-11-19 17:13:07 +1100

    Validation for name and desc

--- ParanoidEnv/ParanoidAndroidProject/paranoidApp/static/paranoidApp/single-page-survey-creation-jquery.js
+++ ParanoidEnv/ParanoidAndroidProject/paranoidApp/static/paranoidApp/single-page-survey-creation-jquery.js
@@ -1,140 +1,150 @@
+var QUESTION_LENGTH = 100;
+
 var survey_json = {};
 var question = [
-    "<div class=\"question\">",
-        "<div>",
-            "<label>Question Text <span class=\"required-marker\">*</span></label>",
-            "<textarea class=\"question-text\"></textarea>",
-        "</div>",
-        "<div>",
-            "<label>Question Name <span class=\"required-marker\">*</span></label>",
-            "<input class=\"question-name\" type=\"text\">",
-        "</div>",
-        "<div>",
-            "<label>Optional Question</label>",
-            "<input class=\"question-optional\" type=\"checkbox\">",
-        "</div>",
-        "<div>",
-            "<label>Question Type</label>",
-            "<select class=\"question-type\">",
-                "<option value=\"text\">Text</option>",
-                "<option value=\"dropdown\">Dropdown</option>",
-                "<option value=\"boolean\">Yes or No</option>",
-                "<option value=\"radio\">Radio Buttons</option>",
-                "<option value=\"number_rating\">Numerical Range</option>",
-                "<option value=\"numerical\">Number</option>",
-                "<option value=\"email\">Email</option>",
-            "</select>",
-        "</div>",
-        "<div class=\"question-type-specific\"></div>",
-        "<p class=\"remove\">Remove Question</p>",
-    "</div>"
-].join("\n");
+    '<div class="question">',
+        '<div>',
+            '<label>Question Text <span class="required-marker">*</span></label>',
+            '<textarea class="question-text"></textarea>',
+            '<p class="survey-error"></p>',
+        '</div>',
+        '<div>',
+            '<label>Question Name <span class="required-marker">*</span></label>',
+            '<input class="question-name" type="text">',
+            '<p class="survey-error"></p>',
+        '</div>',
+        '<div>',
+            '<label>Optional Question</label>',
+            '<input class="question-optional" type="checkbox">',
+        '</div>',
+        '<div>',
+            '<label>Question Type</label>',
+            '<select class="question-type">',
+                '<option value="text">Text</option>',
+                '<option value="dropdown">Dropdown</option>',
+                '<option value="boolean">Yes or No</option>',
+                '<option value="radio">Radio Buttons</option>',
+                '<option value="number_rating">Numerical Range</option>',
+                '<option value="numerical">Number</option>',
+                '<option value="email">Email</option>',
+            '</select>',
+        '</div>',
+        '<div class="question-type-specific"></div>',
+        '<p class="remove">Remove Question</p>',
+    '</div>'
+].join('\n');
 
 
 number_rating_extra = [
-    "<div>",
-        "<label>Minimum Number <span class=\"required-marker\">*</span></label>",
-        "<input type=\"number\" class=\"minimum\" value=\"0\" required>",
-    "</div>",
-    "<div>",
-        "<label>Maximum Number <span class=\"required-marker\">*</span></label>",
-        "<input type=\"number\" class=\"maximum\" value=\"10\" required>",
-    "</div>"
-].join("\n");
+    '<div>',
+        '<label>Minimum Number <span class="required-marker">*</span></label>',
+        '<input type="number" class="minimum" value="0" required>',
+        '<p class="survey-error"></p>',
+    '</div>',
+    '<div>',
+        '<label>Maximum Number <span class="required-marker">*</span></label>',
+        '<input type="number" class="maximum" value="10" required>',
+        '<p class="survey-error"></p>',
+    '</div>'
+].join('\n');
 
 numerical_extra = [
-    "<div>",
-        "<label>Minimum Number</label>",
-        "<input type=\"number\" class=\"minimum\">",
-    "</div>",
-    "<div>",
-        "<label>Maximum Number</label>",
-        "<input type=\"number\" class=\"maximum\">",
-    "</div>"
-].join("\n");
+    '<div class="numerical-extra">',
+        '<div>',
+            '<label>Minimum Number</label>',
+            '<input type="number" class="minimum">',
+        '</div>',
+        '<div>',
+            '<label>Maximum Number</label>',
+            '<input type="number" class="maximum">',
+        '</div>',
+        '<p class="survey-error"></p>',
+    '</div>'
+].join('\n');
 
 multiple_choice_extra_option = [
-    "<div class=\"option\">",
-        "<input type=\"text\" class=\"option-text\"><span class=\"required-marker\">*</span>",
-        "<p class=\"remove-option\">Remove Option</p>",
-    "</div>"
-].join("\n");
+    '<div class="option">',
+        '<input type="text" class="option-text"><span class="required-marker">*</span>',
+        '<p class="survey-error"></p>',
+        '<p class="remove-option">Remove Option</p>',
+    '</div>'
+].join('\n');
 
 
 multiple_choice_extra_start = [
-    "<label>Options:</label>",
-    "<div class=\"options\"></div>",
-    "<p class=\"add-option\">Add an Option</p>"
-].join("\n");
+    '<label>Options:</label>',
+    '<div class="options"></div>',
+    '<p class="add-option">Add an Option</p>'
+].join('\n');
 
 boolean_extra = [
-    "<label>Reveal on:</label>",
-    "<select class=\"boolean-on\">",
-        "<option value=\"true\">Yes</option>",
-        "<option value=\"false\">No</option>",
-    "</select>",
-    "<div class=\"boolean-subquestions\"></div>",
-    "<p class=\"add-subquestion\">Add a Subquestion</p>"
-].join("\n");
+    '<label>Reveal on:</label>',
+    '<select class="boolean-on">',
+        '<option value="true">Yes</option>',
+        '<option value="false">No</option>',
+    '</select>',
+    '<div class="boolean-subquestions"></div>',
+    '<p class="add-subquestion">Add a Subquestion</p>'
+].join('\n');
 
 function RemoveOption($question_type_area)
 {
-    $question_type_area.find(".remove-option").click(function(){
-        $(this).parents(".option").remove();
+    $question_type_area.find('.remove-option').click(function(){
+        $(this).parents('.option').remove();
     });
 }
 
 $(document).ready(function () {
-    $("#add-question").click(function () {
-        $("#questions").append(question);
-        $(".remove").click(function () {
-            $(this).parent(".question").remove();
+    $('#add-question').click(function () {
+        $('#questions').append(question);
+        $('.remove').click(function () {
+            $(this).parent('.question').remove();
         });
-        $(".question-type").change(function(){
-            var $question_type_specific = $(this).parents(".question").find(".question-type-specific");
+        $('.question-type').change(function(){
+            var $question_type_specific = $(this).parents('.question').find('.question-type-specific');
             $question_type_specific.empty();
             var type = $(this).val();
-            if(type == "number_rating"){
+            if(type == 'number_rating'){
                 $question_type_specific.append(number_rating_extra);
             }
-            if(type == "numerical"){
+            if(type == 'numerical'){
                 $question_type_specific.append(numerical_extra);
             }
-            else if(type == "radio" || type == "dropdown"){
+            else if(type == 'radio' || type == 'dropdown'){
                 $question_type_specific.append(multiple_choice_extra_start);
-                $question_type_specific.find(".options").append(multiple_choice_extra_option);
-                $question_type_specific.find(".add-option").click(function(){
-                    $(this).siblings(".options").append(multiple_choice_extra_option);
+                $question_type_specific.find('.options').append(multiple_choice_extra_option);
+                $question_type_specific.find('.add-option').click(function(){
+                    $(this).siblings('.options').append(multiple_choice_extra_option);
                     RemoveOption($question_type_specific);
                 });
                 RemoveOption($question_type_specific);
             }
-            else if(type == "boolean"){
+            else if(type == 'boolean'){
                 $question_type_specific.append(boolean_extra);
-                $question_type_specific.find(".add-subquestion").click(function(){
-                    $(this).siblings(".boolean-subquestions").append(question);
-                    $(this).siblings(".boolean-subquestions").find(".question").each(function(){
-                        $(this).addClass("subquestion");
-                        $(this).removeClass("question");
+                $question_type_specific.find('.add-subquestion').click(function(){
+                    $(this).siblings('.boolean-subquestions').append(question);
+                    $(this).siblings('.boolean-subquestions').find('.question').each(function(){
+                        $(this).addClass('subquestion');
+                        $(this).removeClass('question');
                     });
-                    $(this).siblings(".boolean-subquestions").find(".remove").click(function () {
-                        $(this).parent(".subquestion").remove();
+                    $(this).siblings('.boolean-subquestions').find('.remove').click(function () {
+                        $(this).parent('.subquestion').remove();
                     });
-                    $(".question-type").change(function(){
-                        var $boolean_question_type_specific = $(this).parent("div").siblings(".question-type-specific");
+                    $('.question-type').change(function(){
+                        var $boolean_question_type_specific = $(this).parent('div').siblings('.question-type-specific');
                         $boolean_question_type_specific.empty();
                         var type = $(this).val();
-                        if(type == "number_rating"){
+                        if(type == 'number_rating'){
                             $boolean_question_type_specific.append(number_rating_extra);
                         }
-                        if(type == "numerical"){
+                        if(type == 'numerical'){
                             $boolean_question_type_specific.append(numerical_extra);
                         }
-                        else if(type == "radio" || type == "dropdown"){
+                        else if(type == 'radio' || type == 'dropdown'){
                             $boolean_question_type_specific.append(multiple_choice_extra_start);
-                            $boolean_question_type_specific.find(".options").append(multiple_choice_extra_option);
-                            $boolean_question_type_specific.find(".add-option").click(function(){
-                                $(this).siblings(".options").append(multiple_choice_extra_option);
+                            $boolean_question_type_specific.find('.options').append(multiple_choice_extra_option);
+                            $boolean_question_type_specific.find('.add-option').click(function(){
+                                $(this).siblings('.options').append(multiple_choice_extra_option);
                                 RemoveOption($boolean_question_type_specific);
                             });
                             RemoveOption($boolean_question_type_specific);
@@ -147,83 +157,96 @@ $(document).ready(function () {
 
     
 
-    $("#submit").click(function () {
-        survey_json['name'] = $("#survey-name").val();
-        survey_json['desc'] = $("#survey-desc").val();
+    $('#submit').click(function () {
+        $('.survey-error').empty()
+        error = false;
+        survey_json['name'] = $('#survey-name').val();
+        if(survey_json['name'].length > 200 || survey_json['name'].length <= 0)
+        {
+            $('#survey-name').siblings(".survey-error").html("Name must be between 1 and 200 characters.");
+            error = true;
+        }
+        survey_json['desc'] = $('#survey-desc').val();
+        if(survey_json['desc'].length > 500 || survey_json['desc'].length <= 0)
+        {
+            $('#survey-desc').siblings(".survey-error").html("Description must be between 1 and 500 characters.");
+            error = true;
+        }
+
         survey_json['questions'] = [];
-        $("#questions .question").each(function () {
-            question_column_name = $(this).find(".question-name").val();
-            question_text = $(this).find(".question-text").val();
-            question_type = $(this).find(".question-type").val();
+        $('#questions .question').each(function () {
+            question_column_name = $(this).find('.question-name').val();
+            question_text = $(this).find('.question-text').val();
+            question_type = $(this).find('.question-type').val();
             question_data = {
-                "column-name": question_column_name,
-                "text": question_text,
-                "type": question_type
+                'column-name': question_column_name,
+                'text': question_text,
+                'type': question_type
             }
-            if ($(this).find(".question-optional").is(":checked"))
+            if ($(this).find('.question-optional').is(':checked'))
             {
-                question_data['optional'] = "optional";
+                question_data['optional'] = 'optional';
             }
 
-            if(question_type == "number_rating"){
-                question_data['min'] = $(this).find(".minimum").val();
-                question_data['max'] = $(this).find(".maximum").val();
+            if(question_type == 'number_rating'){
+                question_data['min'] = $(this).find('.minimum').val();
+                question_data['max'] = $(this).find('.maximum').val();
             }
-            if(question_type == "numerical"){
-                min = $(this).find(".minimum").val();
-                max = $(this).find(".maximum").val();
-                if(min != "")
+            if(question_type == 'numerical'){
+                min = $(this).find('.minimum').val();
+                max = $(this).find('.maximum').val();
+                if(min != '')
                 {
                     question_data['min'] = min;
                 }
-                if(max != "")
+                if(max != '')
                 {
                     question_data['max'] = max;
                 }
             }
-            else if(question_type == "radio" || question_type == "dropdown"){
+            else if(question_type == 'radio' || question_type == 'dropdown'){
                 var options = [];
-                $(this).find(".option-text").each(function(){
+                $(this).find('.option-text').each(function(){
                     options.push($(this).val());
                 });
                 question_data['choices'] = options;
             }
-            else if (question_type == "boolean")
+            else if (question_type == 'boolean')
             {  
                 var subquestions = [];
-                $(this).find(".subquestion").each(function(){
-                    subquestion_column_name = $(this).find(".question-name").val();
-                    subquestion_text = $(this).find(".question-text").val();
-                    subquestion_type = $(this).find(".question-type").val();
+                $(this).find('.subquestion').each(function(){
+                    subquestion_column_name = $(this).find('.question-name').val();
+                    subquestion_text = $(this).find('.question-text').val();
+                    subquestion_type = $(this).find('.question-type').val();
                     subquestion_data = {
-                        "column-name": subquestion_column_name,
-                        "text": subquestion_text,
-                        "type": subquestion_type
+                        'column-name': subquestion_column_name,
+                        'text': subquestion_text,
+                        'type': subquestion_type
                     }
-                    if ($(this).find(".question-optional").is(":checked"))
+                    if ($(this).find('.question-optional').is(':checked'))
                     {
-                        subquestion_data['optional'] = "optional";
+                        subquestion_data['optional'] = 'optional';
                     }
 
-                    if(subquestion_type == "number_rating"){
-                        subquestion_data['min'] = $(this).find(".minimum").val();
-                        subquestion_data['max'] = $(this).find(".maximum").val();
+                    if(subquestion_type == 'number_rating'){
+                        subquestion_data['min'] = $(this).find('.minimum').val();
+                        subquestion_data['max'] = $(this).find('.maximum').val();
                     }
-                    else if(subquestion_type == "numerical"){
-                        min = $(this).find(".minimum").val();
-                        max = $(this).find(".maximum").val();
-                        if(min != "")
+                    else if(subquestion_type == 'numerical'){
+                        min = $(this).find('.minimum').val();
+                        max = $(this).find('.maximum').val();
+                        if(min != '')
                         {
                             subquestion_data['min'] = min;
                         }
-                        if(max != "")
+                        if(max != '')
                         {
                             subquestion_data['max'] = max;
                         }
                     }
-                    else if(subquestion_type == "radio" || subquestion_type == "dropdown"){
+                    else if(subquestion_type == 'radio' || subquestion_type == 'dropdown'){
                         var options = [];
-                        $(this).find(".option-text").each(function(){
+                        $(this).find('.option-text').each(function(){
                             options.push($(this).val());
                         });
                         subquestion_data['choices'] = options;
@@ -233,15 +256,17 @@ $(document).ready(function () {
                 if(subquestions.length != 0)
                 {
                     question_data['subquestions'] = subquestions;
-                    question_data['on'] = $(this).find(".boolean-on").val();
+                    question_data['on'] = $(this).find('.boolean-on').val();
                 }
             }
 
             survey_json['questions'].push(question_data);
         });
-        var survey_string = JSON.stringify(survey_json);
-        // console.log(survey_json);
-        $("#hidden-input").val(survey_string);
-        $("#hidden-form").submit();
+        if(!error)
+        {
+            var survey_string = JSON.stringify(survey_json);
+            $('#hidden-input').val(survey_string);
+            $('#hidden-form').submit();
+        }
     });
 });
